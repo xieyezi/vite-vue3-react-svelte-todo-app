9d149cec94f2351615e2f9d73e1b9476cc013626
xieyezi <1435398529@qq.com>
2021-01-27 17:45:00 +0800

    feat:svelte with vite

--- packages/svelte-todo/src/App.svelte
+++ packages/svelte-todo/src/App.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	export let name: string;
+	let logo = './assets/logo.png'
 </script>
 
 <main>
+	<img alt="Vue logo" src={logo} />
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
--- packages/svelte-todo/vite.config.ts
+++ packages/svelte-todo/vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import svelte from 'rollup-plugin-svelte'
 import sveltePreprocess from 'svelte-preprocess'
-import typescript from '@rollup/plugin-typescript'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte({
       preprocess: sveltePreprocess(),
-    }),
-    typescript()
+    })
   ]
 })
