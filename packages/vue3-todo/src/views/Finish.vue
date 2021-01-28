3b962f062bd30350d0c180ba6939de0c7ed73e78
xieyezi <1435398529@qq.com>
2021-01-28 17:36:58 +0800

    feat: add vue3 function

---
+++ packages/vue3-todo/src/views/Finish.vue
@@ -0,0 +1,13 @@
+<template>
+	<p>finish list</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: 'Finish'
+})
+</script>
+
+<style></style>
---
+++ packages/vue3-todo/src/views/Todo.vue
@@ -0,0 +1,13 @@
+<template>
+	<p>todo list</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: 'Todo'
+})
+</script>
+
+<style></style>
