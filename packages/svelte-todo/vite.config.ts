dc08ede0c534b4f0d9fcf4cda48b67bab1a5d257
xieyezi <1435398529@qq.com>
2021-02-04 11:02:28 +0800

    feat: have some isses when run build script svelte build

--- packages/svelte-todo/vite.config.ts
+++ packages/svelte-todo/vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import svelte from 'rollup-plugin-svelte-hot'
-
 import sveltePreprocess from 'svelte-preprocess'
 
-const hot = true
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: './',
 	plugins: [
 		svelte({
 			preprocess: sveltePreprocess()
